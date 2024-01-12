4efd1e9b578e03fdf6777f7bd05c116fdf80b347
Vitalii V.V <vitaliy0895@gmail.com>
2024-01-12 12:01:11 +0300

    refactor: Перенос логики из getData

--- src/model/tradeitModel.ts
+++ src/model/tradeitModel.ts
@@ -1,44 +1,12 @@
 import TradeitAPI from '@src/api/tradeitAPI.ts';
-import type { ICurrenciesResponse, IResponseData } from '@src/model/tradeitModel.types.ts';
+import type {
+  ICurrenciesResponse,
+  IResponseData,
+  ITradeitFilters,
+  ITradeitFiltersCS2,
+  ITradeitFiltersRUST
+} from '@src/model/tradeitModel.types.ts';
 import isNil from 'lodash-es/isNil';
-
-export interface ITradeitFilterBase {
-  offset?: number;
-  limit?: number;
-  sortType?: string;
-  searchValue?: string;
-  minPrice?: number;
-  maxPrice?: number;
-  fresh?: boolean;
-  isForStore?: number;
-}
-
-// TODO: Требуется загружать данные из категории + добавить типы
-export interface ITradeitFiltersCS2 extends ITradeitFilterBase {
-  gameId: 730;
-  minFloat?: number;
-  maxFloat?: number;
-  showTradeLock?: boolean;
-  colors?: string;
-  showUserListing?: boolean;
-  type?: number; // Максимум 17, Others = -1
-  collection?: number; // Максимум 46
-  rarity?: number; // Максимум 11
-  statTrak?: 'Has StatTrak' | 'No StatTrak' | 'Has Stickers';
-  exterior?: 'FN' | 'MW' | 'FT' | 'WW' | 'BS';
-}
-export interface ITradeitFiltersRUST extends ITradeitFilterBase {
-  gameId: 252_490;
-  rustCategory?: string;
-}
-export interface ITradeitFiltersTF2 extends ITradeitFilterBase {
-  gameId: 440;
-}
-export interface ITradeitFiltersSTEAM extends ITradeitFilterBase {
-  gameId: 753;
-}
-
-export type ITradeitFilters = ITradeitFiltersCS2 | ITradeitFiltersRUST | ITradeitFiltersTF2 | ITradeitFiltersSTEAM;
 
 export default class TradeitModel extends TradeitAPI {
   readonly #rustCategory = ['Clothing', 'Weapons', 'Deployable', 'Decorations', 'Misc'];
@@ -73,57 +41,14 @@ export default class TradeitModel extends TradeitAPI {
     url.searchParams.set('limit', limit.toString());
     url.searchParams.set('isForStore', isForStore.toString());
 
-    if (filters.gameId === 730) {
-      const {
-        collection,
-        colors,
-        exterior,
-        maxFloat = 1,
-        minFloat = 0,
-        rarity,
-        showTradeLock = true,
-        showUserListing,
-        statTrak,
-        type
-      } = filters;
-
-      url.searchParams.set('maxFloat', maxFloat < 1 ? maxFloat.toFixed(5) : '1');
-      url.searchParams.set('minFloat', minFloat > 0 ? minFloat.toFixed(5) : '0');
-
-      // Number/Boolean types
-      if (!isNil(rarity)) {
-        url.searchParams.set('rarity', rarity.toString());
+    switch (filters.gameId) {
+      case 730: {
+        this.onChangeDataCS2(filters, url);
+        break;
       }
-      if (!isNil(type)) {
-        url.searchParams.set('type', type.toString());
-      }
-      if (!isNil(collection)) {
-        url.searchParams.set('collection', collection.toString());
-      }
-      if (!isNil(showTradeLock)) {
-        url.searchParams.set('showTradeLock', showTradeLock.toString());
-      }
-      if (!isNil(showUserListing)) {
-        url.searchParams.set('showUserListing', showUserListing.toString());
-      }
-
-      // String types
-      if (colors) {
-        url.searchParams.set('colors', colors);
-      }
-      if (exterior) {
-        url.searchParams.set('exterior', exterior);
-      }
-      if (statTrak) {
-        url.searchParams.set('showUserListing', statTrak);
-      }
-    }
-
-    if (filters.gameId === 252_490) {
-      const { rustCategory } = filters;
-
-      if (rustCategory) {
-        url.searchParams.set('rustCategory', this.getRustId(rustCategory).toString());
+      case 252_490: {
+        this.onChangeDataRUST(filters, url);
+        break;
       }
     }
 
@@ -140,6 +65,7 @@ export default class TradeitModel extends TradeitAPI {
     }
   };
 
+  // TODO: Доработка
   public getCurrencies = async () => {
     try {
       const response = await fetch(this.getPathCurrencies());
@@ -151,6 +77,59 @@ export default class TradeitModel extends TradeitAPI {
       return (await response.json()) as ICurrenciesResponse;
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+
+  public onChangeDataCS2 = (
+    {
+      collection,
+      colors,
+      exterior,
+      maxFloat = 1,
+      minFloat = 0,
+      rarity,
+      showTradeLock = true,
+      showUserListing,
+      statTrak,
+      type
+    }: ITradeitFiltersCS2,
+    url: URL
+  ) => {
+    url.searchParams.set('maxFloat', maxFloat < 1 ? maxFloat.toFixed(5) : '1');
+    url.searchParams.set('minFloat', minFloat > 0 ? minFloat.toFixed(5) : '0');
+
+    // Number/Boolean types
+    if (!isNil(rarity)) {
+      url.searchParams.set('rarity', rarity.toString());
+    }
+    if (!isNil(type)) {
+      url.searchParams.set('type', type.toString());
+    }
+    if (!isNil(collection)) {
+      url.searchParams.set('collection', collection.toString());
+    }
+    if (!isNil(showTradeLock)) {
+      url.searchParams.set('showTradeLock', showTradeLock.toString());
+    }
+    if (!isNil(showUserListing)) {
+      url.searchParams.set('showUserListing', showUserListing.toString());
+    }
+
+    // String types
+    if (colors) {
+      url.searchParams.set('colors', colors);
+    }
+    if (exterior) {
+      url.searchParams.set('exterior', exterior);
+    }
+    if (statTrak) {
+      url.searchParams.set('showUserListing', statTrak);
+    }
+  };
+
+  public onChangeDataRUST = ({ rustCategory }: ITradeitFiltersRUST, url: URL) => {
+    if (rustCategory) {
+      url.searchParams.set('rustCategory', this.getRustId(rustCategory).toString());
     }
   };
 }
--- src/model/tradeitModel.types.ts
+++ src/model/tradeitModel.types.ts
@@ -28,3 +28,41 @@ export interface ICurrenciesResponse {
 export type ICurrencies = Record<ICurrenciesCodes, number>;
 
 export const currenciesToKeep = Object.keys(currenciesSymbol) as ICurrenciesCodes[];
+
+export interface ITradeitFilterBase {
+  offset?: number;
+  limit?: number;
+  sortType?: string;
+  searchValue?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  fresh?: boolean;
+  isForStore?: number;
+}
+
+// TODO: Требуется загружать данные из категории + добавить типы
+export interface ITradeitFiltersCS2 extends ITradeitFilterBase {
+  gameId: 730;
+  minFloat?: number;
+  maxFloat?: number;
+  showTradeLock?: boolean;
+  colors?: string;
+  showUserListing?: boolean;
+  type?: number; // Максимум 17, Others = -1
+  collection?: number; // Максимум 46
+  rarity?: number; // Максимум 11
+  statTrak?: 'Has StatTrak' | 'No StatTrak' | 'Has Stickers';
+  exterior?: 'FN' | 'MW' | 'FT' | 'WW' | 'BS';
+}
+export interface ITradeitFiltersRUST extends ITradeitFilterBase {
+  gameId: 252_490;
+  rustCategory?: string;
+}
+export interface ITradeitFiltersTF2 extends ITradeitFilterBase {
+  gameId: 440;
+}
+export interface ITradeitFiltersSTEAM extends ITradeitFilterBase {
+  gameId: 753;
+}
+
+export type ITradeitFilters = ITradeitFiltersCS2 | ITradeitFiltersRUST | ITradeitFiltersTF2 | ITradeitFiltersSTEAM;
